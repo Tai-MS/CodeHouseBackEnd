814917d171ffde74a5fe88d73700fd08185f35aa
Tai-MS <82743591+Tai-MS@users.noreply.github.com>
2024-01-14 17:32:42 -0300

    Update app.js

--- desafio10-01/app.js
+++ desafio10-01/app.js
@@ -3,15 +3,14 @@ const fs = require('fs')
 
 const app = express()
 
-
-// let productsJson = JSON.parse(fs.readFileSync('./productos.json', 'utf-8'))
-
 app.get('/products', async(req, res) => {
+    //Se intenta leer el archivo products.json, en caso de existir lanzara un error
     try {
         let products = await fs.promises.readFile('./productos.json', 'utf-8')
         let parsedProducts = JSON.parse(products)
         const limit = parseInt(req.query.limit);
-    
+
+        //Si el limite del req.query de la url no un numero, se devolverá el JSON de productos completo 
         if (!isNaN(limit) && limit > 0) {
             let limitedProducts = parsedProducts.slice(0, limit);
             if (limitedProducts) {
@@ -26,11 +25,14 @@ app.get('/products', async(req, res) => {
 });
 
 app.get('/products/:pid', async(req, res) => {
+    //Se intenta leer el archivo products.json, en caso de existir lanzara un error
     try {
         let products = await fs.promises.readFile('./productos.json', 'utf-8')
         let parsedProducts = JSON.parse(products)
         const requestId = req.params.pid;
 
+        //Se busca un ID que coincida con el ingresado en la url
+        //Si no existe, se devolvera un error
         let product = parsedProducts.find((u) => u.id == requestId);
         if (!product) {
             return res.send({ error: "Product not found" });
